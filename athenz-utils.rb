6ac9db73258a76b382336cac8799a131da19135d
Tatsuya Yano <tatyano@yahoo-corp.jp>
2018-04-16 08:30:40 +0900

    Updated version to 1.7.45

--- athenz-utils.rb
+++ athenz-utils.rb
@@ -1,8 +1,8 @@
 class AthenzUtils < Formula
   desc "athenz-utils"
   homepage "https://github.com/yahoo/athenz/tree/master/utils"
-  url "https://bintray.com/yahoo/maven/download_file?file_path=com%2Fyahoo%2Fathenz%2Fathenz-utils%2F1.7.43%2Fathenz-utils-1.7.43-bin.tar.gz"
-  version "1.7.43"
+  url "https://bintray.com/yahoo/maven/download_file?file_path=com%2Fyahoo%2Fathenz%2Fathenz-utils%2F1.7.45%2Fathenz-utils-1.7.45-bin.tar.gz"
+  version "1.7.45"
   sha256 "bd8805426ebc2710a7340145c6d5bea3c22a209b39d122ee9b327e655253a229"
 
   def install
